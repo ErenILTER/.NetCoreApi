0d17ac5fb026a43493ead14babcd45fd1aa76a87
ErenILTER <139958593+ErenILTER@users.noreply.github.com>
2023-07-20 15:36:40 +0300

    .
    .

---
+++ Test/Migrations/20230720080842_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentSurname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.StudentId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ Test/Testing/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.Testing
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string? StudentName { get; set; }
+        public string? StudentSurname { get; set; }
+    }
+}
